210f6dfcdd04ecfda205bfb8d0725547d9de7ed3
cpapanicholas <cpapanicholas@gmail.com>
2023-12-01 11:21:36 -0600

    not full commit yet for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const workouts = require('./Workout');
-const friends = require('./Friends');
+const friend = require('./Friend');
 const Program = require('./Program');
 
 module.exports = { User, workouts, friends, Program};
