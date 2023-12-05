cfad4d729ea7287b0b2e40c6186de58b428cb167
cpapanicholas <cpapanicholas@gmail.com>
2023-12-05 12:45:36 -0600

    fix for my repo

--- Client/utils/mutations.js
+++ Client/utils/mutations.js
@@ -1,83 +1,119 @@
-// mutations.js
 import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      user {
-        username
-        _id
-      }
       token
+      user {
+        _id
+        firstName
+        lastName
+        username
+        email
+        posts {
+          _id
+        }
+        friends {
+          _id
+        }
+        workouts {
+          _id
+        }
+        programs {
+          _id
+        }
+      }
     }
   }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($input: UserInput) {
+  mutation AddUser($input: UserInput) {
     addUser(input: $input) {
       token
     }
   }
 `;
 
-export const ADD_POST = gql`
-  mutation addPost($postText: String!) {
-    addPost(postText: $postText) {
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
       _id
-      postText
-      postAuthor
+      thoughtText
+      thoughtAuthor
       createdAt
       comments {
         _id
         commentText
-        commentAuthor
-        createdAt
       }
     }
   }
 `;
 
 export const ADD_COMMENT = gql`
-  mutation addComment($postId: ID!, $commentText: String!) {
-    addComment(postId: $postId, commentText: $commentText) {
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
       _id
-      postText
-      postAuthor
+      thoughtText
+      thoughtAuthor
       createdAt
       comments {
         _id
         commentText
-        commentAuthor
         createdAt
       }
     }
   }
 `;
 
-export const REMOVE_POST = gql`
-  mutation removePost($postId: ID!) {
-    removePost(postId: $postId) {
+export const UPDATE_WORKOUT = gql`
+  mutation UpdateWorkout($workoutId: ID!, $updatedWorkout: WorkoutInput!) {
+    updateWorkout(workoutId: $workoutId, updatedWorkout: $updatedWorkout) {
       _id
-      postText
-      postAuthor
-      createdAt
+      name
+      workout {
+        phase
+        exercises {
+          exercise {
+            name
+            type
+            muscle
+            equipment
+            difficulty
+            instructions
+          }
+          sets {
+            reps
+            weight
+          }
+        }
+      }
     }
   }
 `;
 
-export const REMOVE_COMMENT = gql`
-  mutation removeComment($postId: ID!, $commentId: ID!) {
-    removeComment(postId: $postId, commentId: $commentId) {
+export const CREATE_WORKOUT = gql`
+  mutation CreateWorkout($workoutInput: CreateWorkoutInput!) {
+    createWorkout(workoutInput: $workoutInput) {
       _id
-      postText
-      postAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
+      name
+      description
+      workout {
+        phase
+        exercises {
+          exercise {
+            name
+            type
+            muscle
+            equipment
+            difficulty
+            instructions
+          }
+          sets {
+            reps
+            weight
+          }
+        }
       }
     }
   }
