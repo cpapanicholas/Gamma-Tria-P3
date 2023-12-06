4d0293e79f2ef83d12497c69505e25cd465e2847
cpapanicholas <cpapanicholas@gmail.com>
2023-12-06 10:25:03 -0600

    commit for socket io

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth')
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { createServer } = require('http'); 
+const { Server } = require('socket.io');  
 
 const PORT = process.env.PORT || 3001;
 const app = express();
