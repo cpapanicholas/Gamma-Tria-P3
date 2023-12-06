f679a443c5547ed4ceda300ca289a74cb81dafe8
cpapanicholas <cpapanicholas@gmail.com>
2023-12-06 16:42:35 -0600

    updated vite.config with a build script

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,6 +14,11 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['@apollo/client'],
+    },
   },  
   test: {
     globals: true,
