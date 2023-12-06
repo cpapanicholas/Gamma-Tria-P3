c8e3d0b6263dc7cadfb75c5005ca9563a5b7e7a5
Tarsum67 <taraum@icloud.com>
2023-12-06 10:58:34 -0600

    added card css

--- Client/src/components/MyPostCard.jsx
+++ Client/src/components/MyPostCard.jsx
@@ -2,19 +2,18 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';export default function MyPostCard ({ post }) {
   return (
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+    <Card style={{ width: '100%' }}>
     <Card.Body>
-      <Card.Title>Card Title</Card.Title>
+      <Card.Title>user</Card.Title>
+    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Text>
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
     </Card.Body>
     <ListGroup className="list-group-flush">
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      <ListGroup.Item>bio</ListGroup.Item>
+      <ListGroup.Item>comment</ListGroup.Item>
     </ListGroup>
     <Card.Body>
       <Card.Link href="#">Card Link</Card.Link>
